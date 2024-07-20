9df81ff3b6467f836959705f7b24f10003a3571c
“[Md <mohdmojammil3@gmail.com>
2024-07-20 00:51:22 -0400

    updating

--- application.py
+++ application.py
@@ -53,7 +53,7 @@ def predict():
     predictions = model.predict(preprocessed_data)
     
     # Return the result
-    return jsonify({'prediction': round(predictions[0], 2)})
+    return jsonify({'prediction': round(predictions[0],)})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
