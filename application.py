72fa0d729681a017e540a70e9bc4d7e3a77b2046
“[Md <mohdmojammil3@gmail.com>
2024-07-20 00:45:41 -0400

    updating html and application.py file

--- application.py
+++ application.py
@@ -4,7 +4,6 @@ import joblib
 import os
 
 application = Flask(__name__)
-
 app = application
 
 # Load the preprocessor and model
@@ -12,6 +11,22 @@ preprocessor_file_path = os.path.join("models", "preprocessor.pkl")
 model_file_path = os.path.join("models", "model.pkl")
 preprocessor = joblib.load(preprocessor_file_path)
 model = joblib.load(model_file_path)
+
+# Dictionary to map month names to integers
+month_mapping = {
+    'January': 1,
+    'February': 2,
+    'March': 3,
+    'April': 4,
+    'May': 5,
+    'June': 6,
+    'July': 7,
+    'August': 8,
+    'September': 9,
+    'October': 10,
+    'November': 11,
+    'December': 12
+}
 
 @app.route('/')
 def home():
@@ -23,7 +38,7 @@ def predict():
     input_data = {
         'Total_Stops': [int(request.form['Total_Stops'])],
         'Airline': [request.form['Airline']],
-        'journey_Month': [int(request.form['journey_Month'])],
+        'journey_Month': [month_mapping[request.form['journey_Month']]],
         'Dep_hour': [int(request.form['Dep_hour'])],
         'Arrival_hour': [int(request.form['Arrival_hour'])],
         'Duration_hours': [int(request.form['Duration_hours'])]
@@ -38,7 +53,7 @@ def predict():
     predictions = model.predict(preprocessed_data)
     
     # Return the result
-    return jsonify({'prediction': round(predictions[0],)})
+    return jsonify({'prediction': round(predictions[0], 2)})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
